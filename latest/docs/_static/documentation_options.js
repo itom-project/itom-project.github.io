0c5e9949848478056b368f3b5ffa62ed14e4bb91
Faulhaber <faulhaber@ito.uni-stuttgart.de>
2020-10-02 14:45:51 +0200

    update latest user doc due to changed source (see commit: 45747d9)

--- latest/docs/_static/documentation_options.js
+++ latest/docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'b167331c',
+    VERSION: '40025785',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
